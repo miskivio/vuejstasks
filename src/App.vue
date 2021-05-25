9d0e2d77c4d439f0328217ae908e675cc1d62c97
sgw8 <meeskeev@gmail.com>
2021-05-25 23:47:03 +0300

    added ref functional to backend

--- src/App.vue
+++ src/App.vue
@@ -35,30 +35,65 @@ export default {
       this.showAddTask = !this.showAddTask
     },
 
-    addTask(task) {
-      this.tasks = [...this.tasks, task]
+    async addTask(task) {
+      const res = await  fetch(`api/tasks`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+             body: JSON.stringify(task)
+      })
+
+      const data = await res.json()
+
+      this.tasks = [...this.tasks, data]
     },
    
-    deleteTask(id) {
+    async deleteTask(id) {
+
       if(confirm('Do you really want to delete this one?')) {
-          this.tasks = this.tasks.filter((task) => task.id !== id)
+        const res = await fetch(`api/tasks/${id}`, {
+          method: 'DELETE'
+        })
+
+          res.status  === 200 ? (this.tasks = this.tasks.filter((task) =>
+           task.id !== id)) : null
         }
     },
 
-toggleReminder (id) {
-  this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task)
-},
+ async toggleReminder (id) {
+  const taskToToggle = await this.fetchTask(id)
+  const updTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+  const res = await fetch(`api/tasks/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(updTask)
+  })
 
+  // back
+  const data = await res.json()
+
+  this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: data.reminder} : task)
+},
 async fetchTasks () {
-    const res = await fetch('http://localhost:5000/tasks')
+    const res = await fetch(`api/tasks`)
     const data = await res.json()
     return data
+    },
+
+async fetchTask (id) {
+    const res = await fetch(`api/tasks/${id}`)
+    const data = await res.json()
+    return data
+    },
+
   },
-async created () {
+
+  async created () {
   this.tasks =  await this.fetchTasks()
-}
-  },
-  
+  }
 }
 
 
